b93cd7f0c965e57c9938c0a1f07e4df14955676a
York lin <superj80820@gmail.com>
2021-07-15 10:48:11 +0800

    fix ioredis connect error

--- storage/redis.js
+++ storage/redis.js
@@ -1,24 +1,27 @@
-const Redis = require('ioredis')
+const Redis = require("ioredis");
 
 const redisFactory = (() => {
   const redis = new Redis({
+    connectTimeout: 10000,
     port: 6379,
     host: process.env.REDIS_HOST,
     family: 4,
     password: process.env.REDIS_PASSWORD,
     db: 0,
-  })
+  });
   return () => ({
     setKey(key, value, setMode = null, expireTime = null) {
-      return setMode && expireTime ? redis.set(key, value, setMode, expireTime) : redis.set(key, value)
+      return setMode && expireTime
+        ? redis.set(key, value, setMode, expireTime)
+        : redis.set(key, value);
     },
     getValueByKey(key) {
-      return redis.get(key)
+      return redis.get(key);
     },
     deleteByKey(key) {
-      return redis.del(key)
+      return redis.del(key);
     },
-  })
-})()
+  });
+})();
 
-module.exports = redisFactory
+module.exports = redisFactory;
