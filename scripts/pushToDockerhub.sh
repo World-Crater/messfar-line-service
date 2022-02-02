c66916c9a5b3aeabc0add524f6007c082facc311
York lin <superj80820@gmail.com>
2022-02-02 16:21:37 +0800

    update docker file for linux

--- scripts/pushToDockerhub.sh
+++ scripts/pushToDockerhub.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker build . -t=superj80820/messfar-line-service
+docker build --platform linux/amd64 . -t=superj80820/messfar-line-service
 docker push superj80820/messfar-line-service
